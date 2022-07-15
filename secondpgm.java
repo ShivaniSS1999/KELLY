104c1fdc41cf1bbd9f863dbed71298b4e5eb66f4
ShivaniSS1999 <109151615+ShivaniSS1999@users.noreply.github.com>
2022-07-15 14:00:16 +0530

    Add files via upload

---
+++ secondpgm.java
@@ -0,0 +1,12 @@
+class secondpgm
+{
+    public static void main(String[] args)
+    {
+        int age =22;
+        System.out.println("age value is " + age);
+         age =25;
+        System.out.println("age  value is" + age);
+         age =30;
+        System.out.println("age  value is" + age);
+    }
+}
